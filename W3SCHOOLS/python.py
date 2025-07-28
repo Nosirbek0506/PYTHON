d00e7aa66fba5e8bd5e9b9ef9c4d8f3f528d0779
Nosirbek0506 <nosirbekyoldoshev@gmail.com>
2025-07-28 15:45:08 +0500

    news

--- W3SCHOOLS/python.py
+++ W3SCHOOLS/python.py
@@ -1,5 +1,18 @@
-rang = ["qora", "oq", "qizil"]
-mashina = ["Spark", "Nexia", "Lacetti"]
-for x in rang:
-    for y in mashina:
-        print(x,y)
+import csv  # csv kutubxonasini chaqiramiz
+
+# Fayl yaratamiz va yozamiz
+with open('talabalar.csv', mode='w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)  # yozuvchi (writer) obyekt
+    writer.writerow(['Ism', 'Yosh', 'Kasb'])  # sarlavhalar
+    writer.writerow(['Ali', 25, 'Dasturchi'])
+    writer.writerow(['Vali', 30, "O'qituvchi"])
+    writer.writerow(['Gulnoza', 22, 'Talaba'])
+
+print("CSV fayl yaratildi va yozildi.")
+
+import pandas as pd
+df=pd.read_csv('talabalar.csv')
+print(df)
+
+print(df.columns)
+print(df['Yosh'],["Ism"])
